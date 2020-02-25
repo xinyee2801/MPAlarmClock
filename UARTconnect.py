6b9ed6b23dbdb43cc5185edb29faf674038576a3
XinYee Lee <48163541+xinyee2801@users.noreply.github.com>
2020-02-25 16:50:13 +0000

    Add files via upload

---
+++ UARTconnect.py
@@ -0,0 +1,33 @@
+import serial
+import numpy as np
+import matplotlib.pyplot as plt
+
+def to_bcd(x):
+    tens = int(np.floor(x / 10)) * 16
+    ones = x - int(np.floor(x / 10))*10
+    print(int(np.floor(x / 10)))
+    total = tens + ones
+    return total.to_bytes(1, 'big')
+    
+serialPort = serial.Serial(port = "COM4", baudrate = 9600, bytesize = 8)
+serialPort.open()
+        
+ #%%
+
+from datetime import datetime
+
+now = datetime.now()
+serialPort.write(to_bcd(now.hour))
+plt.pause(0.2)
+
+now = datetime.now()
+serialPort.write(to_bcd(now.minute))
+plt.pause(0.2)
+
+now = datetime.now()
+serialPort.write(to_bcd(now.second))
+
+
+#%%
+
+serialPort.close()
